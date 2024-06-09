3d489213c3115070c21e75d75a7dfc76c37d0c81
heruv <136377622+heruv@users.noreply.github.com>
2024-06-09 14:17:45 +0300

    feat: making Sierpinski triangle by chaos game v1.0

--- chaos.py
+++ chaos.py
@@ -4,28 +4,34 @@ import matplotlib.pyplot as plt
 
 
 class Fractal:
-    def __init__(self, side,):
+    def __init__(self, side, ):
         self.start_position = None
-        self.P = None
         self.targets = None
         self.A = None
         self.B = None
         self.C = None
-        self.x = None
-        self.y = None
         self.side = side
+
+    def _rotate(self, vector: np.array, angle: int):
+        angle = np.deg2rad(angle)
+        rotation_matrix = np.array([
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle), np.cos(angle)]
+        ])
+
+        rotated_vector = rotation_matrix.dot(vector)
+        rotated_vector[0], rotated_vector[1] = (np.round(rotated_vector[0], 4),
+                                                np.round(rotated_vector[1], 4))
+        return rotated_vector
 
     def vertex_define(self):
         self.A = np.array([0, 0])
         self.B = np.array([self.side, 0])
-        self.C = np.array([self.side / 2, self.side])
-
-        self.x = np.array([self.A[0], self.B[0], self.C[0], self.A[0]])
-        self.y = np.array([self.A[1], self.B[1], self.C[1], self.A[1]])
+        self.C = self._rotate(np.array([self.side - 0, 0]), 60)
 
         self.targets = np.array([self.A, self.B, self.C])
 
-    def triangle_point(self):
+    def get_point_on_triangle(self):
         r1 = random.random()
         r2 = random.random()
 
@@ -33,39 +39,40 @@ class Fractal:
         AB = self.B
 
         if r1 + r2 > 1:  # in auxiliary triangle, perform reflection into target one
-            self.P = np.array([(1-r1)*AC[0] + (1-r2)*AB[0], (1-r1)*AC[1] + (1-r2)*AB[1]])
+            Point = np.array([(1 - r1) * AC[0] + (1 - r2) * AB[0], (1 - r1) * AC[1] + (1 - r2) * AB[1]])
         elif r1 + r2 < 1:
-            self.P = np.array([r1*AC[0] + r2*AB[0], r1*AC[1] + r2*AB[1]])
+            Point = np.array([r1 * AC[0] + r2 * AB[0], r1 * AC[1] + r2 * AB[1]])
+
+        self.start_position = [Point[0], Point[1]]
 
     def move(self):
         target_vertex = random.choice(self.targets)
-        self.start_position = [self.P[0], self.P[1]]
-        position = []
 
-        for i in range(100):
-            movement_distance = np.sqrt(
-                (target_vertex[0] - self.start_position[0]) ** 2 + (target_vertex[1] - self.start_position[1]) ** 2) / 2
+        movement_distance = np.array([(target_vertex[0] - self.start_position[0]) / 2,
+                                      (target_vertex[1] - self.start_position[1]) / 2])
 
-            self.start_position = [self.start_position[0] + movement_distance,
-                                   self.start_position[1] + movement_distance]
+        self.start_position = [self.start_position[0] + movement_distance[0],
+                               self.start_position[1] + movement_distance[1]]
 
-            position.append(self.start_position)
-        print(position)
+        position = self.start_position
+
         return position
 
 
-a = Fractal(10)
+a = Fractal(5)
 a.vertex_define()
-a.triangle_point()
+a.get_point_on_triangle()
 
-plt.plot(a.x, a.y)
+plt.grid(True, which='major', linewidth=1.2)
 r1 = []
 r2 = []
+points = []
 
+for i in range(10_000):
+    points = a.move()
+    r1.append(points[0])
+    r2.append(points[1])
 
-r1.append(a.move()[0])
-r2.append(a.move()[1])
-
-plt.scatter(r1, r2, color='r')
+plt.scatter(r1, r2, s=5, color='r', marker="^")
 plt.axis('equal')
 plt.show()
